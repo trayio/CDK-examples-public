b796459047c33e5c3fad0eb579a33de9deaa8a48
Samzy <6536944+Samzy@users.noreply.github.com>
2024-03-04 10:04:27 +0000

    update connectors json|

--- globalconfig-example/src/GlobalConfig.ts
+++ globalconfig-example/src/GlobalConfig.ts
@@ -1,8 +1,18 @@
 import { OperationGlobalConfigHttp } from "@trayio/cdk-dsl/connector/operation/OperationGlobalConfig";
 import { GlobalConfigExampleAuth } from "./GlobalConfigExampleAuth";
 
+/*
+ * IMPORTANT NOTE: DO NOT DELETE THIS FILE
+ * This is a global configuration that is used by all operations in the connector.
+ * Update the <never> with the right Auth type of the connector.
+ * Update the baseUrl to the base url of the API.
+ * To configure the auth use the withBearerToken method or use addHeader method to add custom headers.
+ *
+ * IMPORTANT NOTE: Do not change the name of the variable `globalConfigHttp` as it is used by the internal Raw Http Operation.
+ * You can ignore this configuration if you have disabled the Raw Http Operation in connector.json
+ */
 export const globalConfigHttp =
   OperationGlobalConfigHttp.create<GlobalConfigExampleAuth>()
-    .withBaseUrl((ctx) => `https://slack.com/api/`)
+    .withBaseUrl((_ctx) => `https://slack.com/api/`)
     .withBearerToken((ctx) => ctx.auth?.user.access_token as string)
-    .addHeader((ctx) => ({ name: "Content-Type", value: "application/json" }));
+    .addHeader((_ctx) => ({ name: "Content-Type", value: "application/json" }));
