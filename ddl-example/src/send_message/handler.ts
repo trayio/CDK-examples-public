77f329496d45bc811799c0c8b00d8b10b13a535b
Samzy <6536944+Samzy@users.noreply.github.com>
2024-03-04 15:54:49 +0000

    update ddl

--- ddl-example/src/send_message/handler.ts
+++ ddl-example/src/send_message/handler.ts
@@ -2,20 +2,21 @@ import { OperationHandlerSetup } from "@trayio/cdk-dsl/connector/operation/Opera
 import { DdlExampleAuth } from "../DdlExampleAuth";
 import { SendMessageInput } from "./input";
 import { SendMessageOutput } from "./output";
+import { globalConfigHttp } from "../GlobalConfig";
 
 export const sendMessageHandler = OperationHandlerSetup.configureHandler<
   DdlExampleAuth,
   SendMessageInput,
   SendMessageOutput
 >((handler) =>
-  handler.usingHttp((http) =>
+  handler.withGlobalConfiguration(globalConfigHttp).usingHttp((http) =>
     http
-      .post("https://slack.com/api/chat.postMessage")
-      .handleRequest((ctx, input, request) => {
-        const access_token = ctx.auth?.user.access_token as string;
-
-        return request.withBearerToken(access_token).withBodyAsJson(input);
+      .post("/chat.postMessage")
+      .handleRequest((_ctx, input, request) => {
+        return request.withBodyAsJson(input);
       })
-      .handleResponse((_ctx, _input, response) => response.parseWithBodyAsJson())
+      .handleResponse((_ctx, _input, response) =>
+        response.parseWithBodyAsJson()
+      )
   )
 );
